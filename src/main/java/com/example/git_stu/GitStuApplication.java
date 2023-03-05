74b5fc60b723363945ddf87d72e71e0d694289ff
Zhang Xingya <2026117540@qq.com>
2023-03-05 10:50:07 +0800

    第一次提交

---
+++ src/main/java/com/example/git_stu/GitStuApplication.java
@@ -0,0 +1,13 @@
+package com.example.git_stu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitStuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitStuApplication.class, args);
+	}
+
+}
